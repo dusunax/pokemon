b09ed9b213e29b8637b377b1257bff854e82f57f
Dusuna <dusunax@gmail.com>
2022-10-21 23:59:24 +0900

    Add Notion env

---
+++ src/pages/api/notion.js
@@ -0,0 +1,11 @@
+// /api/notion
+
+const handler = async (req, res) => {
+  if (req.method === "GET") {
+    const data = req.body;
+
+    res.status(201).json(reponse);
+  }
+};
+
+export default handler;
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -2,7 +2,11 @@ import Head from "next/head";
 
 import BlogPostItems from "../../components/organisms/posts/blog-posts/blog-post-items";
 
+import { TOKEN, DATABASE_ID } from "../../config";
+
 const BlogPage: React.FC = () => {
+  console.log(TOKEN, DATABASE_ID);
+
   return (
     <>
       <Head>
@@ -18,5 +22,26 @@ const BlogPage: React.FC = () => {
     </>
   );
 };
+
+export async function getStaticProps() {
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ page_size: 100 }),
+  };
+
+  fetch("https://api.notion.com/v1/databases/database_id/query", options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+
+  return {
+    props: {},
+  };
+}
 
 export default BlogPage;
