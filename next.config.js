ca476c02332845cdae44634eb00461b9326348f5
Dusuna <dusunax@gmail.com>
2023-01-30 18:45:13 +0900

    add build id

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  onDemandEntries: {
+    maxInactiveAge: 1000 * 60 * 60, // 1 hour
+  },
+  generateBuildId: async () => {
+    return `${Date.now()}`;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
