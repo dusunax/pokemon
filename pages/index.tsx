a81c36fd5345b55a7b4893f32bfa2cf37c5d38b2
Dusuna <dusunax@gmail.com>
2023-01-30 17:10:05 +0900

    Renew create Next app --typescript

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>홈</title>
+      </Head>
+      <main style={{ height: "100vh", textAlign: "center" }}>
+        <h1>hello world</h1>
+      </main>
+    </>
+  );
+}
