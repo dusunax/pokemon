9564d689f513c925887a6e0c75659bca2494270a
Dusuna <dusunax@gmail.com>
2023-02-05 23:53:35 +0900

    react-query devtool 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,25 @@
 import { DefaultLayout } from "@/layouts/DefaultLayout";
 import { Html, Head, Main, NextScript } from "next/document";
 
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "@/react-query/queryClient";
+
 export default function Document() {
   return (
     <Html lang="ko">
       <Head />
       <body>
-        <DefaultLayout>
-          <Main />
-        </DefaultLayout>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools />
+
+          <DefaultLayout>
+            <Main />
+          </DefaultLayout>
+
+          <NextScript />
+        </QueryClientProvider>
       </body>
-      <NextScript />
     </Html>
   );
 }
