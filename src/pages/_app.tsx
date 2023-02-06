1b70bf213c005b4fc11e55aa247ce89bd972e225
Dusuna <dusunax@gmail.com>
2023-02-06 14:00:44 +0900

    react-query devtool 버그 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import "tailwindcss/tailwind.css";
 import "@/styles/index.css";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import type { AppProps } from "next/app";
 
 import { ReactQueryDevtools } from "react-query/devtools";
-import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate, QueryClientProvider } from "react-query";
 import { queryClient } from "@/react-query/queryClient";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -16,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Hydrate state={pageProps.dehydratedState}>
           <Component {...pageProps} />
         </Hydrate>
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </>
   );
