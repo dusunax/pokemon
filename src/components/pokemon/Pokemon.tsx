b0675a20b8a57c43bb07d3b622d098081e48fa56
Dusuna <dusunax@gmail.com>
2023-02-06 14:44:48 +0900

    pokemon 구조 수정

--- src/components/pokemon/Pokemon.tsx
+++ src/components/pokemon/Pokemon.tsx
@@ -1,5 +1,11 @@
 import { useEffect } from "react";
+import Link from "next/link";
+
 import usePoketmonQuery from "./hooks/usePoketmon";
+
+import PokemonNew from "./pokemon-new/PokemonNew";
+import PokemonList from "./pokemon-list/PokemonList";
+import Timer from "../timer/Timer";
 
 export default function Pokemon() {
   const { getPokemonQuery } = usePoketmonQuery();
@@ -11,7 +17,14 @@ export default function Pokemon() {
 
   return (
     <div className="text-center">
-      <h1 className="mb-4 text-4xl font-bold">Pokemon</h1>
+      <div className="flex flex-col gap-4 m-common">
+        <h1 className="mb-4 text-4xl font-bold">Pokemon</h1>
+
+        <PokemonNew />
+        <PokemonList />
+        <Timer />
+        <Link href={`/`}>로그아웃</Link>
+      </div>
     </div>
   );
 }
--- src/pages/pokemon/index.tsx
+++ src/pages/pokemon/index.tsx
@@ -1,9 +1,5 @@
 import Head from "next/head";
-
-import PokemonList from "@/components/pokemon/pokemon-list/PokemonList";
-import PokemonNew from "@/components/pokemon/pokemon-new/PokemonNew";
-import Timer from "@/components/timer/Timer";
-import Link from "next/link";
+import Pokemon from "@/components/pokemon/Pokemon";
 
 export default function PokemonPage() {
   return (
@@ -12,12 +8,7 @@ export default function PokemonPage() {
         <title>포켓몬</title>
       </Head>
       <main>
-        <div className="flex flex-col gap-4 m-common">
-          <PokemonNew />
-          <PokemonList />
-          <Timer />
-          <Link href={`/`}>로그아웃</Link>
-        </div>
+        <Pokemon />
       </main>
     </>
   );
