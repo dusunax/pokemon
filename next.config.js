69f8a42bdb0952700b3fa10dd35e288a90956fca
Dusuna <dusunax@gmail.com>
2023-02-18 23:18:59 +0900

    next.config.js 수정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
-// next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
   reactStrictMode: true,
   onDemandEntries: {
-    maxInactiveAge: 1000 * 60 * 60,
+    maxInactiveAge: 1000 * 60 * 60, // 1 hour
+  },
+  generateBuildId: async () => {
+    return `${Date.now()}`;
   },
   images: {
     domains: ["raw.githubusercontent.com"],
   },
+  allowedExperimental: {
+    images: true,
+  },
 };
+
+module.exports = nextConfig;
