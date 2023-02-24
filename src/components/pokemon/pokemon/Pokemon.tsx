2942f0b8129584cfd7c6ac335bdad9dff95e33e6
Dusuna <dusunax@gmail.com>
2023-02-25 00:10:54 +0900

    리스트 css 조금 수정 : 테두리, hover + opactiy

--- src/components/pokemon/pokemon/Pokemon.tsx
+++ src/components/pokemon/pokemon/Pokemon.tsx
@@ -7,9 +7,15 @@ export default function Pokemon({
   pokemon: PokemonDTO | FBPokemonDTO;
 }) {
   const { names, imgUrl, no } = pokemon;
+  const isCachedRange = no > 0 && no <= 151;
 
   return (
-    <div className="new-pokemon px-2 pt-6 my-4 rounded-full mx-auto bg-[#dde5e4] shadow-inner-custom relative">
+    <div
+      className={`new-pokemon px-2 pt-4 my-2 rounded-full mx-auto bg-[#ebf3f3] grad shadow-inner-custom relative ${
+        isCachedRange && " border-2 border-red"
+      }`}
+    >
+      {/* 포켓몬 이미지 */}
       <Image
         src={imgUrl}
         alt={"뭘까요?"}
@@ -17,8 +23,10 @@ export default function Pokemon({
         height={200}
         className="img w-full object-contain"
       />
-      <div className="name px-2 rounded-lg absolute -top-2 left-1/2 -translate-x-1/2 text-xxs break-keep bg-zinc-500 text-zinc-50">
-        <div className={no > 1 && no <= 151 ? " text-green-300" : ""}>
+
+      {/* 포켓몬 넘버, 포켓몬 이름 */}
+      <div className="name px-2 rounded-lg absolute -top-2 left-1/2 -translate-x-1/2 text-xxs break-keep bg-zinc-500 text-zinc-50 opacity-0">
+        <div className={isCachedRange ? " text-green-300" : ""}>
           {(no > 0 ? no : "") + " " + names["ko"]}
         </div>
       </div>
